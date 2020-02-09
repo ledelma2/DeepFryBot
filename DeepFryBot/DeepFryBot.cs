05b9cb268f60b5adccee6928d9d9d1e7569f890b
dylanrayboss <42479845+dylanrayboss@users.noreply.github.com>
2020-02-09 13:06:27 -0600

    added projects for respective files

---
+++ DeepFryBot/DeepFryBot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepFryBot
+{
+    public class DeepFryBot
+    {
+        private readonly ITranslator translator;
+
+        public DeepFryBot(ITranslator translator)
+        {
+            this.translator = translator;
+        }
+
+        public string DeepFry(string text)
+        {
+            string result = translator.TranslateText(text, "English", "Slovenian");
+            return translator.TranslateText(result, "Slovenian", "English");
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepFryBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeepFryBot deepFryBot = new DeepFryBot(new GoogleTranslator());
+
+            string result = deepFryBot.DeepFry("This is a test string.");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
